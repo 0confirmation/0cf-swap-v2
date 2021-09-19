06f88f20e56e9368c764c6601183b70244ab83dc
Raymond Pulver <ray@Seans-MacBook-Pro.local>
2021-09-19 04:40:47 -0400

    stub implementation of zero keeper available on window.zero

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,18 @@ import { MobxRouter } from 'mobx-router';
 import { Snackbar } from './components/common/Snackbar';
 import { sushiTheme } from './config/themes';
 
+import { createZeroUser, createZeroConnection } from "zero-protocol/dist/lib/zero";
+
+import { InMemoryPersistenceAdapter } from "zero-protocol/dist/lib/persistence/inMemory";
+
+declare global {
+    interface Window { [key: string]: any; }
+}
+(async () => {
+  window.zero = createZeroUser(await createZeroConnection('/dns4/lourdehaufen.dynv6.net/tcp/443/wss/p2p-webrtc-star/'), new InMemoryPersistenceAdapter());
+  window.zero.conn.on('peer:discovery', async (peer: any) => { console.log(peer.peerId); });
+})().catch((err) => console.error(err));
+
 const theme = sushiTheme;
 
 export interface AppProps {
