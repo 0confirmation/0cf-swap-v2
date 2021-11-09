f16406fe27b07ac37ca9c8eb2c26cd3e46bfa70f
flex <flex@flex.gg>
2021-11-09 13:44:45 -0500

    works, check window.user.keepers after init, code in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,26 @@ import { Store, StoreProvider } from './stores/Store';
 import { MobxRouter } from 'mobx-router';
 import { Snackbar } from './components/common/Snackbar';
 import { sushiTheme } from './config/themes';
+import { TrivialUnderwriterTransferRequest, createZeroConnection, createZeroKeeper, createZeroUser } from 'zero-protocol/dist/lib/zero';
+import { Wallet } from "@ethersproject/wallet";
 
 const theme = sushiTheme;
 
 export interface AppProps {
 	store: Store;
 }
+
+const SIGNALING_MULTIADDR = '/dns4/lourdehaufen.dynv6.net/tcp/443/wss/p2p-webrtc-star/';
+
+(async () => {
+  const user = (window as any).user = createZeroUser(await createZeroConnection(SIGNALING_MULTIADDR));
+  const keeper = (window as any).keeper = createZeroKeeper(await createZeroConnection(SIGNALING_MULTIADDR));
+  await Promise.all([ user, keeper ].map(async (v) => await v.conn.start()));
+  const keeperWallet = Wallet.createRandom();
+  await keeper.advertiseAsKeeper(keeperWallet.address);
+  await user.subscribeKeepers();
+})().catch((err) => console.error(err));
+
 
 function App(props: AppProps) {
 	return (
